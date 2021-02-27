3a956e986e30657d85eeacb417c99df5255eece0
Allen West <58156836+allenwest24@users.noreply.github.com>
2021-02-27 18:36:40 -0500

    Create stack.c

---
+++ stack.c
@@ -0,0 +1,36 @@
+/*stack.c*/
+
+/*This program has a buffer overflow vulnerability.*/
+/*Our task is to exploit this vulnerability*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#ifndef BUF_SIZE
+#define BUF_SIZE 256
+#endif
+
+int bof(char*str)
+{
+  char buffer[BUF_SIZE];
+  
+  /*The following statement has a buffer overflow problem*/
+  strcpy(buffer, str);
+  return 1;
+}
+
+int main(int argc, char**argv)
+{
+  char str[517];
+  FILE*badfile;
+  char dummy[BUF_SIZE]; 
+  
+  memset(dummy, 0, BUF_SIZE);
+  badfile = fopen("badfile", "r");
+  fread(str, sizeof(char), 517, badfile);
+  bof(str);
+  printf("Returned Properly\n");
+  
+  return 1;
+}
